888c641a57492e6465891dc9ffffe553c40355dd
bobo <me@bobo.im>
2013-09-07 12:07:00 +0800

    add read_count and comments_count

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,8 @@ class CommentsController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     post.comments.create(comment_params)
-
+    post.comments_count.increment
     redirect_to :back
-    
   end
 
   def comment_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post
   include Mongoid::Document
   include Mongoid::Timestamps::Short
+  include Redis::Objects
+  counter :comments_count
+  counter :read_count
   field :title, type: String
   field :content, type: String
   has_many :comments
